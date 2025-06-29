25243a7941fd9379d0b5115fe5f8dae4a00e5e4c
daichi904 <minimi.904.horo@gmail.com>
2025-06-30 04:29:12 +0900

    refactor a.asm

--- university/computingSystem/MISP/report1/q1/a.asm
+++ university/computingSystem/MISP/report1/q1/a.asm
@@ -1,62 +1,62 @@
-# oddprime.s: 
-#  store all odd primes up to $s0 in memory 
-#  starting at address in $t0
-
-
-.data
-prompt: .asciiz "素数探索の上限値を入力してください: "
-
 .text
 main:
-  # プロンプト表示
-  li   $v0, 4
-  la   $a0, prompt
-  syscall
-
-  # 上限値入力
-  li   $v0, 5
-  syscall
-  move $s0, $v0        # $s0 = 上限値
-
-  # 上限値+2までチェックする
-  addi $s0, $s0, 2
-
-  #  s3を2で初期化。前回の素数。
+  # s0に上限値xを設定 (簡単のため100をハードコーディング)
+  li $s0, 100
+  # 最初の素数として2をs3に設定
   li $s3, 2
-
-  li $t0, 0x1000A000  # base address to store primes
+  # 素数を格納するメモリの基準アドレスを設定
+  li $t0, 0x1000A000
+  # 見つかった双子素数のペアの個数を0で初期化
   li $s1, 0
-  li $s2, 3           # start from 3
+  # 現在調べている奇数を3で初期化
+  li $s2, 3
 
 loop:
-  bgt   $s2, $s0, end # if $s2 > $s0 goto end
-  li    $t1, 3        # divide by >= 3
-is_prime:             # check whether $s2 is prime
-  # bge   $t1, $s2, store # if $t1 >= $s2 go to store
-  bge   $t1, $s2, hutago # if $t1 >= $s2 go to hutago
-  rem   $t2, $s2, $t1 # $t2 = $s2 % $t1 ($t2 = reminder)
-  beq   $t2, $zero, skip  # if $t2==0 then $s2 is not prime
-  addi  $t1, $t1, 2   # add 2 to $t1  
-  j     is_prime      # go back to is_prime
+  # もし、現在調べている数($s2)が上限値($s0)を超えたら終了
+  bgt  $s2, $s0, end
+
+  # 割る数を3で初期化
+  li   $t1, 3
+
+is_prime:
+  # $s2が素数かどうかを判定する
+  # 割る数($t1)が調べている数($s2)以上になったら、素数とみなしhutagoへ
+  bge  $t1, $s2, hutago
+  # $s2を$t1で割った余りを$t2に格納
+  rem  $t2, $s2, $t1
+  # 余りが0なら素数ではないので、次の奇数のチェック(skip)へ
+  beq  $t2, $zero, skip
+  # 割る数を次の奇数へ (t1 = t1 + 2)
+  addi $t1, $t1, 2
+  # is_primeの判定ループに戻る
+  j    is_prime
+
 hutago:
+  # $s2が、$s3(前の素数)の+2かどうかで双子素数かを判定する
   addi $t3, $s3, 2
-  # 現在調べている数が前回調べているものの+2ならstoreへ飛ぶ
-  beq $t3, $s2 store
+  # $s3 + 2 == $s2 なら双子素数なので、storeへ
+  beq  $t3, $s2, store
+  # 双子素数でない場合、現在の素数($s2)を「次の前の素数」として$s3に保存
   move $s3, $s2
-  j skip
+  # 次の奇数のチェック(skip)へ
+  j    skip
 
 store:
-  # 前回の素数を追加
-  sw    $s3, 0($t0)   # store $s2 (since it is prime)
-  addi  $t0, $t0, 4   # increment address
-  # 個数を+1
+  # 双子素数の小さい方($s3)をメモリに格納
+  sw   $s3, 0($t0)
+  # 格納先アドレスを4バイト(1ワード)進める
+  addi $t0, $t0, 4
+  # ペアの個数を1増やす
   addi $s1, $s1, 1
+  # 現在の素数($s2)を次の「前の素数」として$s3に保存
   move $s3, $s2
 
-skip:                 
-  addi  $s2, $s2, 2   # to the next odd number
-  j     loop
+skip:
+  # 次に調べる数を奇数なので2増やす
+  addi $s2, $s2, 2
+  # メインループの先頭に戻る
+  j    loop
 
 end:
-  jr    $ra
-  
+  # 呼び出し元に戻る
+  jr   $ra
